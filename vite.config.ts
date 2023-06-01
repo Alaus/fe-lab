3a1564bfb720bb3375e9fc5526ed88e405d22d64
arafat <arafat@yqsl.onaliyun.com>
2023-06-01 18:07:31 +0800

    feat: 环境变量调试

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import path from 'path'; //这个path用到了安装的@types/node
 
@@ -7,28 +7,31 @@ import Components from 'unplugin-vue-components/vite';
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    AutoImport({
-      resolvers: [ElementPlusResolver()],
-    }),
-    Components({
-      resolvers: [ElementPlusResolver()],
-    }),
-  ],
-  //这里进行配置别名
-  resolve: {
-    alias: {
-      '@': path.resolve('./src'), // @代替src
-      '#': path.resolve('./types'), // #代替types
-    },
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@import "@/assets/styles/index.scss";`,
+export default ({ mode }) => {
+  console.log('mode', loadEnv(mode, process.cwd()).VITE_BASE_URL); //127.0.0.1:9000/api
+  return defineConfig({
+    plugins: [
+      vue(),
+      AutoImport({
+        resolvers: [ElementPlusResolver()],
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()],
+      }),
+    ],
+    //这里进行配置别名
+    resolve: {
+      alias: {
+        '@': path.resolve('./src'), // @代替src
+        '#': path.resolve('./types'), // #代替types
       },
     },
-  },
-});
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@import "@/assets/styles/index.scss";`,
+        },
+      },
+    },
+  });
+};
